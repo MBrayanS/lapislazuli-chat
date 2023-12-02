520ad52c460a97f1e626ed4f27fd764735a33378
MBrayanS <micael.brayan.s@gmail.com>
2023-12-02 13:18:41 -0300

    FEAT: Adicionei a rota de cadastro na API interna

--- src/routes/rotasApi.js
+++ src/routes/rotasApi.js
@@ -1,3 +1,8 @@
 const rotas = require('express').Router()
 
+const ValidacaoController = require('../controllers/ValidacaoController')
+const { UsuarioController } = require('../controllers/ControllersModule')
+
+rotas.post('/cadastrar', ValidacaoController.cadastrar, UsuarioController.cadastrar)
+
 module.exports = rotas
