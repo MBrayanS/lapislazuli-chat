e0be84bb428067a047e5159b48fb7bb20ec2f88e
MBrayanS <micael.brayan.s@gmail.com>
2023-12-02 12:39:23 -0300

    FEAT: Sistema de erros customizados
    Criei a class ErroCustomizado, que extende a class Error, para customizar os erros capturados na aplicação.
    
    Também criei a função middlewareDeErros que tem a responsabilidade de devolver os erros quando eles ocorrerem nas requisições.

--- src/Errors/ErroCustomizado.js
+++ src/Errors/ErroCustomizado.js
@@ -1,7 +1,7 @@
 class ErroCustomizado extends Error {
     constructor( statusCode, message ) {
         super(message)
-        this.statusCode = statusCode || 500
+        this.statusCode = statusCode
     }
 }
 
