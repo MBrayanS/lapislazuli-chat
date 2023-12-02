88f91b9732c8c109a0bd386da17bf9778c3dcc33
MBrayanS <micael.brayan.s@gmail.com>
2023-12-02 13:15:05 -0300

    FEAT: Criei o controller UsuarioController
    Esta middleware lida com as requisições ligadas ao usuario e recebe uma injeção de dependencia de UsuarioService.
    
    Adicionei apenas o método cadastrar até o momento.

---
+++ src/controllers/UsuarioController.js
@@ -0,0 +1,21 @@
+const middlewareDeErros = require("../Errors/middlewareDeErros")
+
+function UsuarioController( UsuarioService ) {
+    async function cadastrar( req, res ) {
+        try {
+            const dadosDeCadastro = req.body
+            
+            await UsuarioService.criar(dadosDeCadastro)
+
+            res.sendStatus(201)
+        }
+        
+        catch( erro ) { middlewareDeErros(erro, res) }
+    }
+
+    return {
+        cadastrar
+    }
+}
+
+module.exports = UsuarioController
