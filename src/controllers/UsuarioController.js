ddbb8bfe010e0f04cb3a4575e2ac60591e3ec121
MBrayanS <micael.brayan.s@gmail.com>
2023-12-07 20:26:58 -0300

    FEAT: Rota para logar usuário

--- src/controllers/UsuarioController.js
+++ src/controllers/UsuarioController.js
@@ -15,8 +15,23 @@ function UsuarioController( UsuarioService, CookieParser, AutenticacaoJWT ) {
         catch( erro ) { middlewareDeErros(erro, res) }
     }
 
+    async function logar( req, res ) {
+        try {
+            const { email, senha } = req.body
+            const { id } = await UsuarioService.pegarPorEmailESenha(email, senha)
+            const token = AutenticacaoJWT.criarToken(id)
+            
+            CookieParser.enviarCookie(token, res)
+
+            res.sendStatus(200)
+        }
+        
+        catch( erro ) { middlewareDeErros(erro, res) }
+    }
+
     return {
-        cadastrar
+        cadastrar,
+        logar
     }
 }
 
