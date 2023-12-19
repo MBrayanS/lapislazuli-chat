14563d10c1d1fa72fb540e29a5b8ed40dc374dc6
MBrayanS <micael.brayan.s@gmail.com>
2023-12-19 20:11:58 -0300

    TEST: Class UsuarioRepository e método buscarPorNome
    Adicionei os testes da class UsuarioRepository.

---
+++ tests/integration/repository/UsuarioRepository.spec.js
@@ -0,0 +1,36 @@
+const UsuarioEntity = require('../../../src/models/UsuarioEntity')
+const { UsuarioRepository } = require('../../../src/modules/RepositoriesModule')
+
+describe('Testando o UsuarioRepository ao', () => {
+
+    const usuariosTestes = {
+        lucas: { nome: 'Lucas', email: 'lucas@gmail.com', senha: 'lucas12!' },
+        ana: { nome: 'Ana', email: 'ana@gmail.com', senha: 'ana1234!' },
+        gabriel: { nome: 'Gabriel', email: 'gabriel@gmail.com', senha: 'gabriel12!' }
+    }
+
+    describe('Buscar usuários pelo nome', () => {
+
+        beforeEach( async () => {
+            await UsuarioRepository.criar(usuariosTestes.lucas)
+            await UsuarioRepository.criar(usuariosTestes.ana)
+            await UsuarioRepository.criar(usuariosTestes.gabriel)
+        })
+
+        afterEach( async () => {
+            await UsuarioEntity.destroy({ where: { email: usuariosTestes.lucas.email } })
+            await UsuarioEntity.destroy({ where: { email: usuariosTestes.ana.email } })
+            await UsuarioEntity.destroy({ where: { email: usuariosTestes.gabriel.email } })
+        }) 
+
+        it('com sucesso', async () => {
+            const usuariosEncontrados = await UsuarioRepository.buscarPorNome('L')
+    
+            expect(usuariosEncontrados[0]).toMatchObject(usuariosTestes.lucas)
+            expect(usuariosEncontrados[1]).toMatchObject(usuariosTestes.gabriel)
+        })
+
+    })
+
+
+})
