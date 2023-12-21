5313f45ba0fbc80a381ca1810dfdf836cff34fdc
MBrayanS <micael.brayan.s@gmail.com>
2023-12-21 14:07:39 -0300

    FEAT: Rota buscarUsuariosPorNome
    Adicionei a rota ao server da aplicação.

--- src/routes/rotasApi.js
+++ src/routes/rotasApi.js
@@ -1,9 +1,12 @@
 const rotas = require('express').Router()
 
+const AutenticacaoController = require('../controllers/AutenticacaoController')
 const ValidacaoController = require('../controllers/ValidacaoController')
+
 const { UsuarioController } = require('../modules/ControllersModule')
 
 rotas.post('/cadastrar', ValidacaoController.cadastrar, UsuarioController.cadastrar)
 rotas.post('/logar', ValidacaoController.logar, UsuarioController.logar)
+rotas.post('/buscarUsuariosPorNome', ValidacaoController.texto, AutenticacaoController.autenticar , UsuarioController.buscarUsuariosPorNome)
 
 module.exports = rotas
