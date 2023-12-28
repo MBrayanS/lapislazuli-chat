9e5008456dfb06bcb140fb7080ebeff5d65806dc
Hugo-Henrique7 <hugo.ccbh7@gmail.com>
2023-12-28 20:39:07 -0300

    Criei a requisição para login

--- public/scripts/login.js
+++ public/scripts/login.js
@@ -1,1 +1,35 @@
-alert('login')
+const enviarForm = document.querySelector('form')
+
+enviarForm.addEventListener('submit', enviarRequisicao)
+
+function enviarRequisicao(evento){
+
+  evento.preventDefault()
+
+  const nomeInput = document.getElementById('loginNome');
+  const senhaInput = document.getElementById('loginSenha');
+
+  const dadosLogin = {
+    nome: nomeInput.value,
+    senha: senhaInput.value
+  }
+  enviarDadosLogin(dadosLogin)
+}
+
+function enviarDadosLogin(dados){
+  fetch('',{
+    method: 'POST',
+    headers:{
+      'Content-Type':'application/json',
+    },
+    body: JSON.stringify(dados)
+  })
+  .then(response => response.json())
+  .then(data =>{
+    console.log('Resposta da Requisição', data);
+  })
+  .catch(error => {
+    console.log('Erro na requisição:', error);
+  })
+}
+
