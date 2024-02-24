762d7509d0d1e61ae3190ba3df1668180c794deb
joaopedrokleinprevidi <kleincoder@gmail.com>
2024-02-24 14:03:14 -0300

    FEAT: Adição do componente Menu adicionar
    Nele contém os botões usados para adicionar novos contatos e criar novos grupos.

--- public/scripts/main.js
+++ public/scripts/main.js
@@ -1,3 +1,24 @@
+const menuDeAdicionarHTML = document.querySelector('#btnMenuAdicionar');
+const btnAbrirFecharMenuAdicionarHTML = document.querySelector('#btnAbrirFecharMenuAdicionar');
+const btnAdicionarContatoHTML = document.querySelector('#btnAdicionarContato')
+const btnCriarGrupoHTML = document.querySelector('#btnCriarGrupo')
+
+btnAbrirFecharMenuAdicionarHTML.addEventListener('click', abrirFecharMenuAdicionar)
+btnAdicionarContatoHTML.addEventListener('click', adicionarContato)
+btnCriarGrupoHTML.addEventListener('click', criarGrupo)
+
+function abrirFecharMenuAdicionar() {
+    menuDeAdicionarHTML.classList.toggle('btn-menu-adicionar--fechado')
+}
+
+function adicionarContato() {
+
+}
+
+function criarGrupo() {
+
+}
+
 async function requisicaoPost( rota, dados ) {
     try {
         const init = {
